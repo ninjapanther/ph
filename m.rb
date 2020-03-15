b1a99566f27a5936fe4351727f782bbba56feb2c
ninjapanther <manjusha.mammen@gmail.com>
2020-03-15 11:10:25 +0530

    modified m.rb to add more error messages for input

--- m.rb
+++ m.rb
@@ -3,7 +3,17 @@ class NumberToWord
 
   def letter_combinations(digits)
     #return if number not valid
-    return ["error"] if digits.nil? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
+	if digits == nil
+		puts "error:digits are nil"
+	elsif digits.length != 10
+		puts "error:It should be a 10-digit number"
+	elsif digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0 
+		puts "numbers have 0 or 1"
+end
+		    
+
+
+return ["error"] if digits.nil? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
     #number to letters mapping
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
 
